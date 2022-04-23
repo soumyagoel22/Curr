b241127d2b8879a68ea4e21068876d9dd5a0dc35
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2022-04-23 15:08:25 +0530

    App

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ const App = () => {
   return (
     <div className="App">
       <h1>Hello</h1>
+      {/* Header */}
+      <div className="app_header">
+        <img
+          className="app_header_image"
+          src="https://www.instagram.com/static/images/web/logged_out_wordmark.png/7a252de00b20.png"
+          alt=""
+        />
+      </div>
+      {/* Post */}
     </div>
   );
 };
